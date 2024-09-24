39bf7d242e1876845137489ed788bf2adae07eac
raghav <raghav03ef@gmail.com>
2024-09-24 23:18:25 +0530

    Initial project setup.

---
+++ server.js
@@ -0,0 +1,20 @@
+var path = require('path');
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.engine('.html', require('ejs').__express);
+app.set('views', path.join(__dirname, '/app/views/'));
+app.set("view engine", "html");
+app.use(express.static(path.join(__dirname, '/app/assets/')));
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(5000, () => {
+    console.log(`server is listening...`);
+});
