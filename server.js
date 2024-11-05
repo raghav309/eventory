3e388c45cd775ad0046d451993d2a79f0f06083a
raghav <raghav03ef@gmail.com>
2024-11-05 14:52:16 +0530

    add prettier formatter and fix icon links in header.html

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var path = require('path');
-const express = require("express");
-const cors = require("cors");
+var path = require('path')
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.engine('.html', require('ejs').__express);
-app.set('views', path.join(__dirname, '/app/views/'));
-app.set("view engine", "html");
-app.use(express.static(path.join(__dirname, '/app/assets/')));
-app.use(cors());
-app.use(express.json());
+app.engine('.html', require('ejs').__express)
+app.set('views', path.join(__dirname, '/app/views/'))
+app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, '/app/assets/')))
+app.use(cors())
+app.use(express.json())
 
-app.use("/", (req, res) => {
-    res.render("pages/index");
-});
+app.use('/', (req, res) => {
+    res.render('pages/index')
+})
 
 app.listen(5000, () => {
-    console.log(`server is listening...`);
-});
+    console.log(`server is listening...`)
+})
