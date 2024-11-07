c212e93f76e212f45d1a45c3ad681fb22bbdf20f
raghav <raghav03ef@gmail.com>
2024-11-07 11:57:09 +0530

    Integrate mongo db and process env variables

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var path = require('path')
 const express = require('express')
-const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const MainRouter = require('./app/routes/main.routes')
 const UserRouter = require('./app/routes/user.routes')
@@ -11,12 +13,19 @@ app.engine('.html', require('ejs').__express)
 app.set('views', path.join(__dirname, '/app/views/'))
 app.set('view engine', 'html')
 app.use(express.static(path.join(__dirname, '/app/assets/')))
-app.use(cors())
 app.use(express.json())
 
 app.use('/', MainRouter)
 app.use('/api/user', UserRouter)
 
-app.listen(5000, () => {
-    console.log(`server is listening...`)
-})
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('DB connection established')
+        app.listen(process.env.PORT, () => {
+            console.log(`server is listening on ${process.env.PORT}...`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
